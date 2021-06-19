e65c2709a6cf1f2520f167bff70f87d426b424c5
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-06-19 15:54:35 +0200

    Update allHostPortScan.sh

--- allHostPortScan.sh
+++ allHostPortScan.sh
@@ -15,8 +15,8 @@ if [ $1 ]; then
     for host in ${hosts[@]}; do
         echo -e "\n\t[>] $host\n"
         for port in $(seq 1 65535); do
-            timeout 1 bash -c "echo '' > /dev/tcp/$host/$port"  2>/dev/null && echo "\t\t[*]$host ==> Port $port - OPEN" &
-        done; wait
+            timeout 1 bash -c "echo '' > /dev/tcp/$host/$port"  2>/dev/null && echo "\t\t[*]$host ==> Port $port - OPEN"
+        done
     done
 else
     echo -e "\n[*] Usage:  ./allHostPortScan.sh  <ip-address> <ip-address> <ip-address> <ip-address> <ip-address>\n"
