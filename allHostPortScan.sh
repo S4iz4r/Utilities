89cdc3f42a4cff8575dc590d4c83447ee20996a4
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-06-19 02:12:01 +0200

    Update allHostPortScan.sh

--- allHostPortScan.sh
+++ allHostPortScan.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 function ctrl_c() {
-	echo -e "\n[!] Exiting..."
-	tput cnorm; exit 1
+    echo -e "\n[!] Exiting..."
+    tput cnorm; exit 1
 }
 trap crtl_c INT
 
 tput civis
 
-hosts=(172.18.0.1 172.18.0.2 172.18.0.3 172.18.0.4)
+hosts=($1 $2 $3 $4 $5)
 
-for host in ${hosts[@]}; do
-	for port in $(seq 1 200); do
-		timeout 1 bash -c "echo '' > /dev/tcp/$host/$port"  2>/dev/null && echo "[*]$host ==> Port $port - OPEN" &
-	done; wait
-done
+if [ $1 ]; then
+    for host in ${hosts[@]}; do
+        for port in $(seq 1 65535); do
+            timeout 1 bash -c "echo '' > /dev/tcp/$host/$port"  2>/dev/null && echo "[*]$host ==
+> Port $port - OPEN" &
+        done; wait
+    done
+else
+    echo -e "\n[*] Uso:  ./allHostPortScan.sh  <ip-address> <ip-address> <ip-address> <ip-address> <ip-address>\n"
+    tput cnorm
+    exit 1
+fi
+tput cnorm
