924100068425630c97c5d2f2028a972927e2952e
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-06-19 22:40:39 +0200

    Update portScan.sh

--- portScan.sh
+++ portScan.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# ./portScan.sh <ip-address>
+function ctrl_c() {
+    echo -e "\n[!] Exiting..."
+    exit 1
+}
+trap ctrl_c INT
 
 if [ $1 ]; then
 	ip_address=$1
@@ -8,6 +12,6 @@ if [ $1 ]; then
 		timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port"  2>/dev/null && echo "[*] Port $port - OPEN" &
 	done; wait
 else
-	echo -e "\n[*] Uso:  ./portScan.sh  <ip-address>\n"
+	echo -e "\n[*] Usage:  ./portScan.sh  <ip-address>\n"
 	exit 1
 fi
