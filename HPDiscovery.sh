df5f9faf215a800e075a29cdbf1e1e45044da24e
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-06-19 22:36:35 +0200

    Update and rename HostandPortDiscovery.sh to HPDiscovery.sh

--- HPDiscovery.sh
+++ HPDiscovery.sh
@@ -13,16 +13,16 @@ if [[ $1 == *.*.*. ]] && [[ $2 ]]; then
     echo -e "[*] Scanning Hosts for $ip_address??"
     hosts+=($(for i in $(seq 2 254); do timeout 1 bash -c "ping -c 1 -I $2 $ip_address$i > /dev/null  2>&1" && echo "$ip_address$i" & done; wait))
     echo -e "\n[*] Detected active hosts:\n $(for i in ${hosts[@]}; do echo -e "\t\t$i"; done)\n"
-    echo -e "\t[*] Scanning ports"
+    echo -e "[*] Scanning ports"
     for host in ${hosts[@]}; do
-        echo -e "\t\t[>] $host"
-        for port in $(seq 1 10000); do
-            timeout 1 bash -c "echo '' > /dev/tcp/$host/$port" 2>/dev/null && echo -e "\t\t\t[*] $host ==> Port $port - OPEN" 
-        done  
+        echo -e "\n\t[>] $host\n"
+        for port in $(seq 1 1000); do
+            timeout 1 bash -c "echo '' > /dev/tcp/$host/$port" 2>/dev/null && echo -e "\t\t[*] Port $port - OPEN"
+        done
     done
 else
-    echo -e "\n[!] Usage:  HostandPortDiscovery.sh  <ip-address --.--.--.> <interface>\n"
-    echo -e "\n[!] Example: ./HostandPortDiscovery.sh 10.10.10. tun0\n"
+    echo -e "\n[!] Usage:  HPDiscovery.sh  <ip-address --.--.--.> <interface>\n"
+    echo -e "\n[!] Example: ./HPDiscovery.sh 127.0.0. eth0\n"
     tput cnorm; exit 1
 fi
 
