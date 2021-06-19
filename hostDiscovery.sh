8296c959870b0eecc8db041004382320f6e220d9
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-06-19 22:53:56 +0200

    Update hostDiscovery.sh

--- hostDiscovery.sh
+++ hostDiscovery.sh
@@ -6,7 +6,7 @@ if [[ $1 == *.*.*. ]]; then
 		timeout 1 bash -c "ping -c 1 $ip_address$i > /dev/null  2>&1" && echo "Host $ip_address$i - ACTIVE" &
 	done; wait
 else
-	echo -e "\n[*] Uso:  hostDyscovery.sh  <ip-address --.--.--.>\n"
+	echo -e "\n[*] Usage:  ./hostDyscovery.sh  <ip-address --.--.--.>\n"
 	exit 1
 fi
 
