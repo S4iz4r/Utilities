c41d815f5405fc79568141ef29c534c801743773
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-06-20 18:43:18 +0200

    Update HPDiscovery.sh

--- HPDiscovery.sh
+++ HPDiscovery.sh
@@ -15,7 +15,7 @@ if [[ $1 == *.*.*. ]] && [[ $2 ]]; then
     echo -e "[*] Scanning Hosts for $ip_address??"
     hosts+=($(for i in $(seq 2 254); do timeout 1 bash -c "ping -c 1 -I $2 $ip_address$i > /dev/null  2>&1" && echo "$ip_address$i" & done; wait))
     echo -e "\n[*] Detected active hosts:\n $(for i in ${hosts[@]}; do echo -e "\t\t$i"; done)\n"
-    echo -e "[*] Scanning 1 to $portRange OPEN ports"
+    echo -e "[*] Scanning from port 1 to port $portRange"
     for host in ${hosts[@]}; do
         ttl=$(timeout 1 bash -c "ping -c 1 -I $2 $host | grep ttl= | awk -F= '{print \$3,\$4}' | sed 's/ //3; s/time/ /' 2>&1") && echo -e "\n$host   ttl=${ttl}\n"
         for port in $(seq 1 $portRange); do
