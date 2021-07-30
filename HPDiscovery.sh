e4898b77a8174b51fb4cc5f71e3aead75b0f55b0
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2021-07-30 09:21:36 +0200

    Update HPDiscovery.sh
    Added conditional to check if network interface configuration utility is "ifconfig" or "ip"

--- HPDiscovery.sh
+++ HPDiscovery.sh
@@ -10,6 +10,12 @@ tput civis
 portRangeStart=1
 portRange=5000
 
+if ! which ifconfig >/dev/null; then
+    net=$(which ip)" a show"
+else
+    net=$(which ifconfig)
+fi
+
 if [[ $1 == *.*.*. ]] && [[ $2 ]]; then
     if [[ $3 ]] && [[ -z $4 ]]; then
         portRange=$3
@@ -23,9 +29,8 @@ if [[ $1 == *.*.*. ]] && [[ $2 ]]; then
     hosts+=($(for i in $(seq 2 254); do timeout 0.5 bash -c "ping -c 1 -I $2 $ip_address$i > /dev/null  2>&1" && echo "$ip_address$i" & done; wait))
     echo -e "\n[*] Detected active hosts:\n $(for i in ${hosts[@]}; do echo -e "\t\t$i"; done)\n"
     echo -e "[*] Scanning open ports     [[   from port   $portRangeStart   to port   $portRange   ]]"
-    sleep 2
     for host in ${hosts[@]}; do
-        if [[ $host != $(/usr/sbin/ifconfig $2 | grep "inet " | awk '{print $2}') ]]; then
+        if [[ $host != $($net $2 | grep "inet " | awk '{print $2}') ]]; then
             ttl=$(bash -c "ping -c 1 -I $2 $host | grep ttl= | awk -F= '{print \$3,\$4}' | sed 's/ //3; s/time/ /' 2>&1") && echo -e "\n$host   ttl=${ttl}\n"
             for port in $(seq $portRangeStart $portRange); do
                 timeout 0.5 bash -c "echo '' > /dev/tcp/$host/$port" 2>/dev/null && echo -e "\t\tPort $port" &
@@ -34,10 +39,10 @@ if [[ $1 == *.*.*. ]] && [[ $2 ]]; then
     done
 else
     echo -e "\n[!] Usage:  ./HPDiscovery.sh  <ip-address --.--.--.> <interface> <start port> <finish port>"
-    echo -e "\n[!] Port scanner range set from 1 to 5000 by deffault"
-    echo -e "\n[!] Example: ./HPDiscovery.sh 127.0.0. eth0"
-    echo -e "\n[!] Example: ./HPDiscovery.sh 127.0.0. eth0 65535"
-    echo -e "\n[!] Example: ./HPDiscovery.sh 127.0.0. eth0 1000 5000\n"
+    echo -e "[!] Port scanner range set from 1 to 5000 by deffault"
+    echo -e "[!] Example: ./HPDiscovery.sh 127.0.0. eth0"
+    echo -e "[!] Example: ./HPDiscovery.sh 127.0.0. eth0 65535"
+    echo -e "[!] Example: ./HPDiscovery.sh 127.0.0. eth0 1000 5000\n"
     tput cnorm; exit 1
 fi
 
