dbc64a8a8c7734292e5b7287d88ba6c221289a36
S4iz4r <jabiersaizar@gmail.com>
2024-02-11 00:39:20 +0100

    add example regex and text

--- regex_checker.py
+++ regex_checker.py
@@ -2,6 +2,30 @@ import re
 import tkinter as tk
 
 
+def examples():
+    regex_entry.delete(0, "end")
+    text_entry.delete('1.0', "end")
+    regex_entry.insert(
+        0, "((https?):((//)|(\\\\))+([a-zA-Z0-9\d:#@%/;$()~_?\+\-=\\\.&](#!)?)*)")
+    text_entry.insert(
+        '1.0', """This regex above is to check if URL exist in the text.
+        
+        This is an example text, this text contains emails, phone numbers and other sample designed for
+        testing purposes. To use this tool, please type a regular expression in the text-area above.
+        
+        When designing regex for emails make sure you cover all possible email types like:
+        info@breatheco.de, dragon23@gmail.com, dragon_ball@yahoo.com.us and also test for bad email
+        formats like ramond=32@skas.com
+        
+        When texting for urls you have samples like this: https://thedomain.com.ve/dir1/dir2.html, some
+        urls don't have extensions like this http://www.thedomain.net/directory1, maybe you will find some
+        urls with nested subdomains like http://test.www.thedomain.com.ve/directory1
+        
+        Credits: Thanks to Florian Dedov for inspiring me to create this simple but entertaining project.
+        Sample text extracted from: https://4geeks.com/lesson/regex-tutorial-regular-expression-examples""")
+    find_matches('')
+
+
 def find_matches(event):
     text_entry.tag_delete("match")
     error_label.config(text='')
@@ -28,7 +52,9 @@ def find_matches(event):
 
 root = tk.Tk()
 root.title('Regex Checker')
-root.config(bg='grey')
+menu_bar = tk.Menu()
+menu_bar.add_command(label='Example Regex/Text', command=examples)
+root.config(bg='grey', menu=menu_bar)
 
 
 regex_label = tk.Label(root, text='Enter Regex:',
@@ -44,7 +70,7 @@ text_label = tk.Label(root, text='Enter Text:',
                       font=('Arial', 15, 'bold'), bg='grey', fg='black')
 text_label.grid(row=1, column=0, padx=5, pady=5)
 
-text_entry = tk.Text(root, width=80, height=15,
+text_entry = tk.Text(root, width=80, height=20,
                      bg='lightgrey', font=('Arial', 15), fg='black')
 text_entry.grid(row=1, column=1, padx=5, pady=5, columnspan=2)
 text_entry.bind('<KeyRelease>', find_matches)
@@ -55,7 +81,7 @@ description.grid(row=2, column=0, padx=5, pady=5)
 matches_description = tk.Label(root, text='Yellow background for all matches', font=(
     'Arial', 12, 'bold'), bg='yellow', fg='black')
 matches_description.grid(row=2, column=1, padx=5, pady=5, sticky='e')
-match_all_text = tk.Label(root, text='Orange background for specific match', font=(
+match_all_text = tk.Label(root, text='Orange background for whole text match', font=(
     'Arial', 12, 'bold'), bg='orange', fg='black')
 match_all_text.grid(row=2, column=2, padx=5, pady=5, sticky='w')
 
